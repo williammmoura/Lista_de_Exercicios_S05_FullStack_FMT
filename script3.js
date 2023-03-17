05612a2f745766ede2b0c9ebae2dca323cfde12d
William Monte de Moura <williamonte@gmail.com>
2023-03-17 17:47:31 -0300

    Exercício 04

--- script3.js
+++ script3.js
@@ -5,11 +5,23 @@
 */
 
 let arrayNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let arrayInvertido = [];
+// let arrayInvertido = [];
 
-const inverterArray = arrayNumeros => {
-    return arrayNumeros.reverse();
-} 
+// const inverterArray = arrayNumeros => {
+//     return arrayNumeros.reverse();
+// } 
 
-console.log(arrayNumeros);
-console.log(inverterArray(arrayNumeros));
+// console.log(arrayNumeros);
+// console.log(inverterArray(arrayNumeros));
+
+
+//Exercício 04
+/* Utilizando o mesmo vetor ‘arrayNumeros’, que possui valores de 1 a 9. Crie agora uma variável chamada ‘arrayImpares’.
+Utilizando a funcionalidade de arrays ‘filter’ em ‘arrayNumeros’, crie uma arrow function que teste se um valor passado por parâmetro é ímpar    (retornando 1 ou true se for ímpar, 0 ou false se for par) e passe esta função como parâmetro de ‘filter’. Por fim, confira se o vetor ‘arrayImpares’ possui todos os números ímpares presentes em ‘arrayNumeros’.
+*/
+let arrayImpares = [];
+
+const arrayImpar = impar => impar.filter(numImpar => numImpar % 2 != 0);
+
+console.log(arrayNumeros); 
+console.log(arrayImpar(arrayNumeros));
