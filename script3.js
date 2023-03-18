e5ee47d5df30dcc5254f6c54d525a70d03bb5ea9
William Monte de Moura <williamonte@gmail.com>
2023-03-18 14:03:37 -0300

    Exercício 05

--- script3.js
+++ script3.js
@@ -19,9 +19,24 @@ let arrayNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 /* Utilizando o mesmo vetor ‘arrayNumeros’, que possui valores de 1 a 9. Crie agora uma variável chamada ‘arrayImpares’.
 Utilizando a funcionalidade de arrays ‘filter’ em ‘arrayNumeros’, crie uma arrow function que teste se um valor passado por parâmetro é ímpar    (retornando 1 ou true se for ímpar, 0 ou false se for par) e passe esta função como parâmetro de ‘filter’. Por fim, confira se o vetor ‘arrayImpares’ possui todos os números ímpares presentes em ‘arrayNumeros’.
 */
-let arrayImpares = [];
+// let arrayImpares = [];
 
-const arrayImpar = impar => impar.filter(numImpar => numImpar % 2 != 0);
+// const arrayImpar = impar => impar.filter(numImpar => numImpar % 2 != 0);
 
-console.log(arrayNumeros); 
-console.log(arrayImpar(arrayNumeros));
+// console.log(arrayNumeros); 
+// console.log(arrayImpar(arrayNumeros));
+
+
+//Exercício 05
+/* Utilizando o mesmo vetor ‘arrayNumeros’, que possui valores de 1 a 9. Crie agora uma variável chamada ‘arrayQuadrados’.
+Utilizando a funcionalidade de arrays ‘map’ em ‘arrayNumeros’, crie uma arrow function que calcule o quadrado de um valor passado por parâmetro e passe esta função como parâmetro de ‘map’. Por fim, confira se o vetor ‘arrayQuadrados’ possui todos os quadrados dos números presentes em ‘arrayNumeros’ e exiba-os no console.
+*/
+
+let arrayQuadrados = [];
+
+const numQuadrados = arrayNumeros => arrayNumeros.map(num => {
+    return num ** 2;
+});
+
+console.log(arrayNumeros)
+console.log(numQuadrados(arrayNumeros));
